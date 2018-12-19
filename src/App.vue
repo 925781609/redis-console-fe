6e546aecb1e882ddc4ec722c0328c84e8fa49c4a
yang.liu <yang.liu@hypers.com>
2018-12-19 14:47:51 +0800

    feat: 增加导航侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>RedisConsole</el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
+        <el-aside width="200px">
+          <navmenu></navmenu>
+        </el-aside>
+        <el-main>执行命令</el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
+// @ is alias for src defined in webpack.base.conf.js
+import NavMenu from '@/components/NavMenu'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'navmenu': NavMenu
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="24">
+      <h5>实例信息</h5>
+      <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>Redis key查询</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">Cluster</template>
+            <el-menu-item index="1-1">cluster1</el-menu-item>
+            <el-menu-item index="1-2">cluster2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Sentinel">
+            <el-menu-item index="1-3">sentinel1</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">Standalone</template>
+            <el-menu-item index="1-4-1">standalone1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">查找</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span slot="title">设置</span>
+        </el-menu-item>
+
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu',
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
